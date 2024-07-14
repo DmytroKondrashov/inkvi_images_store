15351458d49b89512af9aeccc8cc9f683e2ade10
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-14 11:30:58 +0300

    implement CreateUserDTO

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDTO } from './dto/create.user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('/create')
-  async createUser(@Body() body) {
+  async createUser(@Body() body: CreateUserDTO) {
     return this.userService.createUser(body.email, body.password);
   }
 }
