18fcf65065955bce4686a50780838acce56218d8
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-19 22:48:08 +0300

    add JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserService } from 'src/user/user.service';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: process.env.JVT_SECRET,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
   providers: [AuthService, UserService, JwtService, LocalStrategy],
   controllers: [AuthController],
 })
