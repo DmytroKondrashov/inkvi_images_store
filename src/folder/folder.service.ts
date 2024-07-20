ccdfd1049734c560cb277deef6cea3eeb23ccff8
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-20 21:25:45 +0300

    inject folder repository

--- src/folder/folder.service.ts
+++ src/folder/folder.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Folder } from './entity/folder.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class FolderService {}
+export class FolderService {
+  constructor(
+    @InjectRepository(Folder)
+    private readonly userRepository: Repository<Folder>,
+  ) {}
+}
