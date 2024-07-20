ad0827b5c32d8c9807fa27cabdd36f28b7ac5078
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-20 22:32:44 +0300

    add createFolder action

--- src/folder/folder.service.ts
+++ src/folder/folder.service.ts
@@ -1,21 +1,19 @@
-import { Body, Injectable, Post } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
 import { Folder } from './entity/folder.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Public } from 'src/common/decorators/public.decorator';
 import { CreateFolderrDTO } from './dto/create.folder.dto';
 
 @Injectable()
 export class FolderService {
   constructor(
     @InjectRepository(Folder)
-    private readonly userRepository: Repository<Folder>,
-    private readonly folderService: FolderService,
+    private readonly folderRepository: Repository<Folder>,
   ) {}
 
-  @Public()
-  @Post()
-  async createFolder(@Body() body: CreateFolderrDTO) {
-    return this.folderService.createFolder(body);
+  async createFolder(body: CreateFolderrDTO) {
+    const { userId, name } = body;
+    const folder = this.folderRepository.create({ userId, name });
+    return this.folderRepository.save(folder);
   }
 }
