f0e6f3fdf9c2a1960e64a775ad43fbc1da025d50
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-20 15:58:11 +0300

    add /create folder route

--- src/folder/folder.controller.ts
+++ src/folder/folder.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { FolderService } from './folder.service';
+import { CreateFolderrDTO } from './dto/create.folder.dto';
 
 @Controller('folder')
 export class FolderController {
   constructor(private readonly folderService: FolderService) {}
+
+  @Post('/create')
+  async createFolder(@Body() body: CreateFolderrDTO) {
+    return this.folderService.createFolder(body);
+  }
 }
