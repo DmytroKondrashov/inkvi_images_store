65f0a84cd8e9f886640acdd453c637e48803b9fd
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-21 20:51:48 +0300

    add createImage function

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Image } from './entity/image.entity';
 
 @Injectable()
-export class ImageService {}
+export class ImageService {
+  constructor(
+    @InjectRepository(Image)
+    private readonly imageRepository: Repository<Image>,
+  ) {}
+
+  async createImage(image: Buffer): Promise<Image> {
+    const newImage = this.imageRepository.create({ image });
+    return this.imageRepository.save(newImage);
+  }
+}
