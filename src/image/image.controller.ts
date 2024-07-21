d7aaf80e3d86abbbbf715d861febef6315ce651e
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-21 20:54:31 +0300

    add uploadFile action

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ImageService } from './image.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('image')
-export class ImageController {}
+export class ImageController {
+  constructor(private readonly imageService: ImageService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const savedImage = await this.imageService.createImage(file.buffer);
+    return savedImage;
+  }
+}
