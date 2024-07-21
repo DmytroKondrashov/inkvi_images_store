6517ee87fb994bb39a856317c414a48e6908da1e
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-21 20:33:21 +0300

    import Folder repository

--- src/folder/folder.module.ts
+++ src/folder/folder.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FolderController } from './folder.controller';
 import { FolderService } from './folder.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Folder } from './entity/folder.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Folder])],
   controllers: [FolderController],
-  providers: [FolderService]
+  providers: [FolderService],
 })
 export class FolderModule {}
