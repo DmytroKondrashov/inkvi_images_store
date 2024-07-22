2aeab07b0f2426e6717acca6d8c1583eaac09444
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-22 21:24:33 +0300

    add filename to entity

--- src/image/entity/image.entity.ts
+++ src/image/entity/image.entity.ts
@@ -1,9 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Image {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  @IsString()
+  @IsNotEmpty()
+  filename: string;
 
   @Column({ type: 'longblob', nullable: false })
   image: Buffer;
