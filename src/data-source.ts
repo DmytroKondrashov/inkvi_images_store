6ca716c0a0f0bc278aefbf6e94ef44bed3fd0370
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-24 22:02:23 +0300

    update data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DATABASE_USERNAME || 'root',
   password: process.env.DATABASE_PASSWORD || 'root',
   database: process.env.DATABASE_NAME || 'inkvi_images_store',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  entities: [__dirname + '/**/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   synchronize: false,
 });
