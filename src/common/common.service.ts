d7d6119201bf1af7d72d2611eadaf03e216b3c35
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-26 21:35:28 +0300

    add getUserIdFromToken method

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class CommonService {}
+export class CommonService {
+  constructor(private jwtService: JwtService) {}
+
+  async getUserIdFromToken(token: string): Promise<string> {
+    try {
+      const decoded = this.jwtService.verify(token, {
+        secret: process.env.JWT_SECRET,
+      });
+      return decoded.sub;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
+  }
+}
