456144f34e8c330e3f90af5090d90034afa61c0e
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-27 15:35:38 +0300

    use user uniqueness guerd

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDTO } from './dto/create.user.dto';
 import { Public } from 'src/common/decorators/public.decorator';
 import { UpdateUserDTO } from './dto/update.user.dto';
 import { Token } from 'src/common/decorators/token.decorator';
+import UserUniquenessGuard from './guards/user.uniqueness.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Public()
+  @UseGuards(UserUniquenessGuard)
   @Post('/create')
   async createUser(@Body() body: CreateUserDTO) {
     return this.userService.createUser(body);
