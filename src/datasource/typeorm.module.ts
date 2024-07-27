bcde5c4ddfa9b2b572bb905280e3a95ff8892cb5
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-27 15:29:35 +0300

    add entities

--- src/datasource/typeorm.module.ts
+++ src/datasource/typeorm.module.ts
@@ -1,5 +1,8 @@
 import { DataSource } from 'typeorm';
 import { Global, Module } from '@nestjs/common';
+import { User } from 'src/user/entity/user.entity';
+import { Folder } from 'src/folder/entity/folder.entity';
+import { Image } from 'src/image/entity/image.entity';
 
 @Global()
 @Module({
@@ -17,7 +20,7 @@ import { Global, Module } from '@nestjs/common';
             username: process.env.DATABASE_USERNAME || 'root',
             password: process.env.DATABASE_PASSWORD || 'root',
             database: process.env.DATABASE_NAME || 'inkvi_images_store',
-            entities: [__dirname + '/**/**/*.entity{.ts,.js}'],
+            entities: [User, Image, Folder],
             migrations: [__dirname + '/migrations/*{.ts,.js}'],
             synchronize: false,
           });
