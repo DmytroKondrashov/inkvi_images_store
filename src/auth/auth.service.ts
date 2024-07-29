28b0e0a3092aadd18f25057cdee4ae99445a33ea
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-29 21:33:35 +0300

    update login function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { UserService } from '../user/user.service';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/user/entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private userService: UserService,
     private jwtService: JwtService,
+    private readonly userRepository: Repository<User>,
   ) {}
 
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.id };
-    return {
-      access_token: this.jwtService.sign(payload, {
-        secret: process.env.JWT_SECRET,
-      }),
-    };
+  async login(user) {
+    const foundUser = this.userRepository.findOne({
+      where: { email: user.email },
+    });
+    if (foundUser) {
+      const payload = { sub: user.id, email: user.email };
+      return {
+        token: this.jwtService.sign(payload, {
+          secret: process.env.JWT_SECRET,
+        }),
+      };
+    } else {
+      throw new BadRequestException('User not found!');
+    }
   }
 }
