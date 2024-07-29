3aaf40bee5becd50a72caea8a3073a5220fa292d
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-29 22:23:39 +0300

    create UserDTO

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class UserDTO {
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  emails: string;
+}
