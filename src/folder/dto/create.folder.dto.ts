4b75e1e7050bb235e7488a1a35f5b553845e40cd
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-03 21:00:33 +0300

    remove user id

--- src/folder/dto/create.folder.dto.ts
+++ src/folder/dto/create.folder.dto.ts
@@ -1,10 +1,6 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateFolderrDTO {
-  @IsNotEmpty()
-  @IsNumber()
-  userId: number;
-
   @IsNotEmpty()
   @IsString()
   name: string;
