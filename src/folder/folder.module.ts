ae29dc8fc21e71d0f01554a079bfb2d6383f03da
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-03 21:41:59 +0300

    fix imports

--- src/folder/folder.module.ts
+++ src/folder/folder.module.ts
@@ -3,10 +3,17 @@ import { FolderController } from './folder.controller';
 import { FolderService } from './folder.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Folder } from './entity/folder.entity';
+import { CommonService } from 'src/common/common.service';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/user/entity/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Folder])],
+  imports: [
+    TypeOrmModule.forFeature([Folder]),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [FolderController],
-  providers: [FolderService],
+  providers: [FolderService, CommonService, UserService, JwtService],
 })
 export class FolderModule {}
