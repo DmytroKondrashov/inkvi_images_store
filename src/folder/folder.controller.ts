20e25332e41cb323c326931a07abb7a0ae1bd483
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-03 22:26:57 +0300

    add edit folter functionality

--- src/folder/folder.controller.ts
+++ src/folder/folder.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { FolderService } from './folder.service';
 import { CreateFolderrDTO } from './dto/create.folder.dto';
 import { Token } from 'src/common/decorators/token.decorator';
+import { UpdateFolderrDTO } from './dto/update.folder.dto';
 
 @Controller('folder')
 export class FolderController {
@@ -10,5 +11,10 @@ export class FolderController {
   @Post('/create')
   async createFolder(@Body() body: CreateFolderrDTO, @Token() token: string) {
     return this.folderService.createFolder(body, token);
+  }
+
+  @Post('/edit')
+  async editFolder(@Body() body: UpdateFolderrDTO) {
+    return this.folderService.editFolder(body);
   }
 }
--- src/folder/folder.service.ts
+++ src/folder/folder.service.ts
@@ -1,19 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Folder } from './entity/folder.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateFolderrDTO } from './dto/create.folder.dto';
+import { CommonService } from 'src/common/common.service';
+import { UserService } from 'src/user/user.service';
+import { UpdateFolderrDTO } from './dto/update.folder.dto';
 
 @Injectable()
 export class FolderService {
   constructor(
     @InjectRepository(Folder)
     private readonly folderRepository: Repository<Folder>,
+    private readonly commonService: CommonService,
+    private readonly userService: UserService,
   ) {}
 
-  async createFolder(body: CreateFolderrDTO) {
-    const { userId, name } = body;
-    const folder = this.folderRepository.create({ userId, name });
+  async createFolder(body: CreateFolderrDTO, token: string) {
+    const { name } = body;
+    const userId = await this.commonService.getUserIdFromToken(token);
+    const user = await this.userService.getUser(userId);
+    const folder = this.folderRepository.create({ user, name });
     return this.folderRepository.save(folder);
+  }
+
+  async editFolder(body: UpdateFolderrDTO) {
+    try {
+      const { id, name } = body;
+      await this.folderRepository.update(id, { name });
+      return this.folderRepository.findOne({ where: { id } });
+    } catch (error) {
+      throw new BadRequestException('Could not edit folder');
+    }
   }
 }
