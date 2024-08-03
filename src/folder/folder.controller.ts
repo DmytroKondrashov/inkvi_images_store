8f34e575b1cbab04bc2ae3e6a83a63d35022be0f
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-03 21:26:45 +0300

    add token

--- src/folder/folder.controller.ts
+++ src/folder/folder.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { FolderService } from './folder.service';
 import { CreateFolderrDTO } from './dto/create.folder.dto';
+import { Token } from 'src/common/decorators/token.decorator';
 
 @Controller('folder')
 export class FolderController {
   constructor(private readonly folderService: FolderService) {}
 
   @Post('/create')
-  async createFolder(@Body() body: CreateFolderrDTO) {
-    return this.folderService.createFolder(body);
+  async createFolder(@Body() body: CreateFolderrDTO, @Token() token: string) {
+    return this.folderService.createFolder(body, token);
   }
 }
