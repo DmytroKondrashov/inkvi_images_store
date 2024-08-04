05a6630030e14ae5594c63608666c36df762bcac
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-04 12:34:29 +0300

    create ManipulateOwnFolderGuard

---
+++ src/folder/guards/manipulate.own.folder.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { UserService } from '../../user/user.service';
+import { FolderService } from '../folder.service';
+import { CommonService } from 'src/common/common.service';
+
+@Injectable()
+export default class ManipulateOwnFolderGuard implements CanActivate {
+  constructor(
+    private readonly userService: UserService,
+    private readonly folredService: FolderService,
+    private readonly commonService: CommonService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    if (!request.headers.authorization) {
+      return false;
+    }
+    const token = request.headers.authorization.split(' ')[1];
+    const userId = await this.commonService.getUserIdFromToken(token);
+    const { id } = request.body;
+    const folder = await this.folredService.getFolder(id);
+    return userId === folder.user.id;
+  }
+}
