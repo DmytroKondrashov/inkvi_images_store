56fee9dc505b2cbb720fca6a22bc565fcbac6f32
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-04 12:40:54 +0300

    @UseGuards(ManipulateOwnFolderGuard) use

--- src/folder/folder.controller.ts
+++ src/folder/folder.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { FolderService } from './folder.service';
 import { CreateFolderrDTO } from './dto/create.folder.dto';
 import { Token } from 'src/common/decorators/token.decorator';
 import { UpdateFolderrDTO } from './dto/update.folder.dto';
+import ManipulateOwnFolderGuard from './guards/manipulate.own.folder.guard';
 
 @Controller('folder')
 export class FolderController {
@@ -14,11 +15,13 @@ export class FolderController {
   }
 
   @Post('/edit')
+  @UseGuards(ManipulateOwnFolderGuard)
   async editFolder(@Body() body: UpdateFolderrDTO) {
     return this.folderService.editFolder(body);
   }
 
   @Post('/delete')
+  @UseGuards(ManipulateOwnFolderGuard)
   async deleteFolder(@Body() id: number) {
     return this.folderService.deleteFolder(id);
   }
