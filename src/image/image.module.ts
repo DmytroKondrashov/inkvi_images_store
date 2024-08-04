9685d43c3ef1d1bc3494e576f6342215f72f92e8
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-04 21:30:46 +0300

    fix imports

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -3,10 +3,22 @@ import { ImageController } from './image.controller';
 import { ImageService } from './image.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Image } from './entity/image.entity';
+import { FolderService } from 'src/folder/folder.service';
+import { CommonService } from 'src/common/common.service';
+import { UserService } from 'src/user/user.service';
+import { Folder } from 'src/folder/entity/folder.entity';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/user/entity/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Image])],
+  imports: [TypeOrmModule.forFeature([Image, Folder, User])],
   controllers: [ImageController],
-  providers: [ImageService],
+  providers: [
+    ImageService,
+    FolderService,
+    CommonService,
+    UserService,
+    JwtService,
+  ],
 })
 export class ImageModule {}
