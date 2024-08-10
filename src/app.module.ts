93ed23f0003686262bd19549fd9836db9a2752eb
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-10 12:23:47 +0300

    add basic d configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,27 @@ import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { FolderModule } from './folder/folder.module';
 import { ImageModule } from './image/image.module';
-import { TypeOrmModule } from './datasource/typeorm.module';
 import { CommonModule } from './common/common.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeorm from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, FolderModule, ImageModule, TypeOrmModule, CommonModule],
+  imports: [
+    UserModule,
+    FolderModule,
+    ImageModule,
+    CommonModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
