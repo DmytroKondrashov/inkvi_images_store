d91660f78a3e2892cbe526fab5e656d28572ab1f
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-12 10:29:49 +0300

    return id

--- src/folder/dto/update.folder.dto.ts
+++ src/folder/dto/update.folder.dto.ts
@@ -1,6 +1,10 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class UpdateFolderDTO {
+  @IsNotEmpty()
+  @IsNumber()
+  id: number;
+
   @IsNotEmpty()
   @IsString()
   name: string;
