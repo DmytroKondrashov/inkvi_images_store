25ff1eb949b3853a53b6c2fc38e7934a178bf460
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-16 23:02:08 +0300

    fix imports

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TagController } from './tag.controller';
 import { TagService } from './tag.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tag } from './entity/tag.entity';
+import { Image } from 'src/image/entity/image.entity';
+import { CommonService } from 'src/common/common.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Tag, Image])],
   controllers: [TagController],
-  providers: [TagService]
+  providers: [TagService, CommonService, JwtService],
 })
 export class TagModule {}
