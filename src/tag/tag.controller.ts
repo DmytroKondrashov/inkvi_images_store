390e3b0c1ca07423c475ccb26dcad862421594f2
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-16 22:33:52 +0300

    add basic crud actions

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TagService } from './tag.service';
+import { CreateTagDTO } from './dto/create.tag.dto';
+import { UpdateTagDTO } from './dto/update.tag.dto';
 
 @Controller('tag')
-export class TagController {}
+export class TagController {
+  constructor(private readonly tagService: TagService) {}
+
+  @Post('/create')
+  async createFolder(@Body() body: CreateTagDTO) {
+    return this.tagService.createTag(body);
+  }
+
+  @Post('/edit')
+  async editFolder(@Body() body: UpdateTagDTO) {
+    return this.tagService.editTag(body);
+  }
+
+  @Post('/delete')
+  async deleteFolder(@Body() id: number) {
+    return this.tagService.deleteTag(id);
+  }
+
+  @Get('/tags')
+  async getFolders() {
+    return this.tagService.getTags();
+  }
+}
