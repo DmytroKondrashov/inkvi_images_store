973daa8364314f06c8e824bf0a73e6098551adf5
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-25 21:37:31 +0300

    comment temporary migrations

--- src/migrations/1723970149534-AddTagEntity.ts
+++ src/migrations/1723970149534-AddTagEntity.ts
@@ -1,15 +1,15 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+// import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddTagEntity1723707130556 implements MigrationInterface {
-  name = 'AddTagEntity1723707130556';
+// export class AddTagEntity1723707130556 implements MigrationInterface {
+//   name = 'AddTagEntity1723707130556';
 
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `CREATE TABLE \`tag\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
-    );
-  }
+//   public async up(queryRunner: QueryRunner): Promise<void> {
+//     await queryRunner.query(
+//       `CREATE TABLE \`tag\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+//     );
+//   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE \`tag\``);
-  }
-}
+//   public async down(queryRunner: QueryRunner): Promise<void> {
+//     await queryRunner.query(`DROP TABLE \`tag\``);
+//   }
+// }
