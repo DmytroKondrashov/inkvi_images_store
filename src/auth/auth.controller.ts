fbb6b09a7c2bba780adaaf2ce8de8df5d2d44731
DmytroKondrashov <kondrashovd101@gmail.com>
2024-08-26 11:39:40 +0300

    update post login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Request, Post, Get, Render } from '@nestjs/common';
+import {
+  Controller,
+  Request,
+  Post,
+  Get,
+  Render,
+  Response,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from 'src/common/decorators/public.decorator';
 import { LoginDTO } from './dto/login.dto';
@@ -16,7 +23,14 @@ export class AuthController {
 
   @Public()
   @Post('login')
-  async login(@Request() req: LoginDTO) {
-    return this.authService.login(req);
+  async login(@Request() req: LoginDTO, @Response() res) {
+    const token = await this.authService.login(req);
+    res.cookie('token', token, {
+      httpOnly: true, // Secure flag to prevent JavaScript access to the cookie
+      secure: process.env.NODE_ENV === 'production', // Use secure flag in production
+      maxAge: 3600 * 1000, // 1 hour expiration
+      sameSite: 'strict', // Prevent CSRF attacks
+    });
+    return res.redirect('/folders_list');
   }
 }
