17681a52e5882ff486949e3004258de79cc6222c
DmytroKondrashov <kondrashovd101@gmail.com>
2024-09-10 13:09:20 +0300

    add Image many-to-many

--- src/tag/entity/tag.entity.ts
+++ src/tag/entity/tag.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Image } from '../../image/entity/image.entity';
 
 @Entity()
 export class Tag {
@@ -7,4 +8,7 @@ export class Tag {
 
   @Column()
   name: string;
+
+  @ManyToMany(() => Image, (image) => image.tags)
+  images: Image[];
 }
